0c4282a1f36a6e32b81a8027c7d827f958668f3d
anguila09 <45208326+anguila09@users.noreply.github.com>
2019-08-31 18:20:44 -0300

    formulario de contacto de prueba

--- pages/contacto/index.vue
+++ pages/contacto/index.vue
@@ -1,14 +1,83 @@
 <template>
-    <form name="contact" netlify>
-    <p>
-        <label>Name <input type="text" name="name" /></label>
-    </p>
-    <p>
-        <label>Email <input type="email" name="email" /></label>
-    </p>
-    <p>
-        <button type="submit">Send</button>
-    </p>
-    </form>
-
+  <mdb-container>
+    <h1 class="h1-responsive font-weight-bold text-center my-5">Solicitud de Insumos Distritales</h1>
+    <div class="row">
+      <div class="col-lg-9">
+        <mdb-row>
+            <mdb-col>
+              <mdb-input label="Your name" v-model="fields.name" required />
+            </mdb-col>
+            <mdb-col>
+              <mdb-input type="email" label="Your email" v-model="fields.email" required />
+            </mdb-col>
+          </mdb-row>
+          <mdb-row>
+            <mdb-col>
+              <mdb-input label="Subject" v-model="fields.subject" required />
+            </mdb-col>
+          </mdb-row>
+          <mdb-row>
+            <mdb-col>
+              <mdb-input
+                type="textarea"
+                label="Your message"
+                v-model="fields.message"
+                required
+              />
+            </mdb-col>
+          </mdb-row>
+        </mdb-col>
+        <mdb-btn color="primary" type="submit" class="float-left">Solicitar</mdb-btn>
+      </div>
+    </div>
+  </mdb-container>
 </template>
+
+
+
+<script>
+  import {
+    mdbContainer,
+    mdbInput,
+    mdbCard,
+    mdbCardHeader,
+    mdbCardTitle,
+    mdbCardText,
+    mdbCardBody,
+    mdbIcon,
+    mdbBtn,
+    mdbRow,
+    mdbCol
+  } from "mdbvue";
+
+  export default {
+    components: {
+      mdbContainer,
+      mdbInput,
+      mdbCard,
+      mdbCardHeader,
+      mdbCardTitle,
+      mdbCardBody,
+      mdbCardText,
+      mdbIcon,
+      mdbBtn,
+      mdbRow,
+      mdbCol
+    },
+    data() {
+      return {
+        fields: {
+          name: "",
+          email: "",
+          subject: "",
+          message: ""
+        }
+      };
+    },
+      head() {
+        return {
+          title: "Quienes Somos"
+        };
+      }
+  };
+</script>
